8b4285ddc8acfbe3d988749c1ca45b4657dca0c2
Yaguay <yaelaguayo@gmail.com>
2019-08-23 12:36:20 -0700

    Update Program.cs
    Hola TEC.

--- P1/Program.cs
+++ P1/Program.cs
@@ -6,7 +6,7 @@ namespace P1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola TEC");
         }
     }
 }
